311032a13f94d87579bb6305715a8e57c4d2be6f
Tomasz Pęczek <tpeczek@gmail.com>
2022-06-21 00:27:34 +0200

    Composition via Ajax

---
+++ 03-composition-via-ajax/Demo.AspNetCore.MicroFrontendsInAction.Inspire/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Rewrite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions()
+    .AddRedirect("recommendations/(.*).html", "recommendations/$1")
+    .AddRewrite("recommendations/(.*)", "recommendations/$1.html", skipRemainingRules: true)
+    .AddRedirect("fragment/recommendations/(.*).html", "fragment/recommendations/$1")
+    .AddRewrite("fragment/recommendations/(.*)", "fragment/recommendations/$1.html", skipRemainingRules: true));
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ServeUnknownFileTypes = true,
+    OnPrepareResponse = (ctx) =>
+    {
+        ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "http://localhost:3001");
+        ctx.Context.Response.Headers.Append("Access-Control-Allow-Headers", "*");
+        ctx.Context.Response.Headers.Append("Access-Control-Allow-Methods", "GET");
+    }
+});
+
+app.Run();
