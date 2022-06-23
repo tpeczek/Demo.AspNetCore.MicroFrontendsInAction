26385623e0788204710ccf27e5f8b9dc8b582fbf
Tomasz Pęczek <tpeczek@gmail.com>
2022-06-23 23:38:33 +0200

    Composition via Ajax

---
+++ 03-composition-via-ajax/Demo.AspNetCore.MicroFrontendsInAction.Decide/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.AspNetCore.MicroFrontendsInAction.Decide.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Eicher()
+        {
+            return View();
+        }
+
+        public IActionResult Fendt()
+        {
+            return View();
+        }
+
+        public IActionResult Porsche()
+        {
+            return View();
+        }
+    }
+}
