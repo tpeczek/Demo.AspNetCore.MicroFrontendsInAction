d7183db8ad0a5edf48f22f4a8c84b0c03d53c495
David Fowler <davidfowl@gmail.com>
2022-06-28 11:25:50 -0400

    Update Program.cs

--- 04-server-side-routing-via-yarp/Demo.AspNetCore.MicroFrontendsInAction.Proxy/Program.cs
+++ 04-server-side-routing-via-yarp/Demo.AspNetCore.MicroFrontendsInAction.Proxy/Program.cs
@@ -9,19 +9,15 @@ var app = builder.Build();
 string decideServiceUrl = app.Configuration["DECIDE_SERVICE_URL"];
 string inspireServiceUrl = app.Configuration["INSPIRE_SERVICE_URL"];
 
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    // Per service prefixes
-    endpoints.MapForwarder("/", decideServiceUrl);
+// Per service prefixes
+app.MapForwarder("/", decideServiceUrl);
 
-    // Per service prefixes
-    endpoints.MapForwarder("/decide/{**catch-all}", decideServiceUrl);
-    endpoints.MapForwarder("/inspire/{**catch-all}", inspireServiceUrl);
+// Per service prefixes
+app.MapForwarder("/decide/{**catch-all}", decideServiceUrl);
+app.MapForwarder("/inspire/{**catch-all}", inspireServiceUrl);
 
-    // Per page prefixes
-    endpoints.MapForwarder("/product/{**catch-all}", decideServiceUrl);
-    endpoints.MapForwarder("/recommendations/{**catch-all}", inspireServiceUrl);
-});
+// Per page prefixes
+app.MapForwarder("/product/{**catch-all}", decideServiceUrl);
+app.MapForwarder("/recommendations/{**catch-all}", inspireServiceUrl);
 
 app.Run();
