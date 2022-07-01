9f277058d324803ccc38789fce64830834052def
Tomasz Pęczek <tpeczek@gmail.com>
2022-07-01 17:30:54 +0200

    [Composition via YARP] Switch HTTP Forwarder to Reverse Proxy

---
+++ 05_06.01-composition-via-yarp-and-ssi/Demo.AspNetCore.MicroFrontendsInAction.Inspire/Controllers/RecommendationsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.AspNetCore.MicroFrontendsInAction.Inspire.Controllers
+{
+    public class RecommendationsController : Controller
+    {
+        public IActionResult Eicher()
+        {
+            return View();
+        }
+
+        public IActionResult Fendt()
+        {
+            return View();
+        }
+
+        public IActionResult Porsche()
+        {
+            return View();
+        }
+    }
+}
---
+++ 05_06.01-composition-via-yarp-and-ssi/Demo.AspNetCore.MicroFrontendsInAction.Proxy/Program.cs
@@ -0,0 +1,31 @@
+using Yarp.ReverseProxy.Configuration;
+using Demo.AspNetCore.MicroFrontendsInAction.Proxy;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var routes = new[]
+{
+    // Per service prefixes
+    new RouteConfig { RouteId = Constants.ROOT_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/" } },
+    // Per service prefixes
+    new RouteConfig { RouteId = Constants.DECIDE_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/decide/{**catch-all}" } },
+    new RouteConfig { RouteId = Constants.INSPIRE_ROUTE_IDE, ClusterId = Constants.INSPIRE_CLUSTER_ID, Match = new RouteMatch { Path = "/inspire/{**catch-all}" } },
+    // Per page prefixes
+    new RouteConfig { RouteId = Constants.PRODUCT_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/product/{**catch-all}" } },
+    new RouteConfig { RouteId = Constants.RECOMMENDATIONS_ROUTE_ID, ClusterId = Constants.INSPIRE_CLUSTER_ID, Match = new RouteMatch { Path = "/recommendations/{**catch-all}" } }
+};
+
+var clusters = new[]
+{
+    new ClusterConfig() { ClusterId = Constants.DECIDE_CLUSTER_ID, Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase) { { Constants.DECIDE_SERVICE_URL, new DestinationConfig() { Address = builder.Configuration[Constants.DECIDE_SERVICE_URL] } } } },
+    new ClusterConfig() { ClusterId = Constants.INSPIRE_CLUSTER_ID, Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase) { { Constants.INSPIRE_SERVICE_URL, new DestinationConfig() { Address = builder.Configuration[Constants.INSPIRE_SERVICE_URL] } } } }
+};
+
+builder.Services.AddReverseProxy()
+    .LoadFromMemory(routes, clusters);
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
