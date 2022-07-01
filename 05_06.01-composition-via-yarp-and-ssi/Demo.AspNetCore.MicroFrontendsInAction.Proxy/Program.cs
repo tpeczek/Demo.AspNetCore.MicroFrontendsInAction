d8a7ad06353327e0a4a7f1c36787cdd6e9f0b980
Tomasz Pęczek <tpeczek@gmail.com>
2022-07-01 17:40:41 +0200

    [Composition via YARP and SSI] Adding a no-op body transform

--- 05_06.01-composition-via-yarp-and-ssi/Demo.AspNetCore.MicroFrontendsInAction.Proxy/Program.cs
+++ 05_06.01-composition-via-yarp-and-ssi/Demo.AspNetCore.MicroFrontendsInAction.Proxy/Program.cs
@@ -1,17 +1,18 @@
 using Yarp.ReverseProxy.Configuration;
 using Demo.AspNetCore.MicroFrontendsInAction.Proxy;
+using Demo.AspNetCore.MicroFrontendsInAction.Proxy.Transforms;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var routes = new[]
 {
     // Per service prefixes
-    new RouteConfig { RouteId = Constants.ROOT_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/" } },
+    new RouteConfig { RouteId = Constants.ROOT_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/" }, Metadata = SsiTransformProvider.SsiEnabledMetadata },
     // Per service prefixes
-    new RouteConfig { RouteId = Constants.DECIDE_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/decide/{**catch-all}" } },
+    new RouteConfig { RouteId = Constants.DECIDE_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/decide/{**catch-all}" }, Metadata = SsiTransformProvider.SsiEnabledMetadata },
     new RouteConfig { RouteId = Constants.INSPIRE_ROUTE_IDE, ClusterId = Constants.INSPIRE_CLUSTER_ID, Match = new RouteMatch { Path = "/inspire/{**catch-all}" } },
     // Per page prefixes
-    new RouteConfig { RouteId = Constants.PRODUCT_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/product/{**catch-all}" } },
+    new RouteConfig { RouteId = Constants.PRODUCT_ROUTE_ID, ClusterId = Constants.DECIDE_CLUSTER_ID, Match = new RouteMatch { Path = "/product/{**catch-all}" }, Metadata = SsiTransformProvider.SsiEnabledMetadata },
     new RouteConfig { RouteId = Constants.RECOMMENDATIONS_ROUTE_ID, ClusterId = Constants.INSPIRE_CLUSTER_ID, Match = new RouteMatch { Path = "/recommendations/{**catch-all}" } }
 };
 
@@ -22,7 +23,8 @@ var clusters = new[]
 };
 
 builder.Services.AddReverseProxy()
-    .LoadFromMemory(routes, clusters);
+    .LoadFromMemory(routes, clusters)
+    .AddTransforms<SsiTransformProvider>();
 
 var app = builder.Build();
 
---
+++ 05_06.01-composition-via-yarp-and-ssi/Demo.AspNetCore.MicroFrontendsInAction.Proxy/Transforms/SsiTransformProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Transforms.Builder;
+
+namespace Demo.AspNetCore.MicroFrontendsInAction.Proxy.Transforms
+{
+    internal class SsiTransformProvider : ITransformProvider
+    {
+        private const string SSI_METADATA_FLAG = "SSI";
+        private const string SSI_METADATA_FLAG_ON = "ON";
+
+        public static IReadOnlyDictionary<string, string> SsiEnabledMetadata { get; } = new Dictionary<string, string>() { { SSI_METADATA_FLAG, SSI_METADATA_FLAG_ON } };
+
+        public void ValidateRoute(TransformRouteValidationContext context)
+        { }
+
+        public void ValidateCluster(TransformClusterValidationContext context)
+        { }
+
+        public void Apply(TransformBuilderContext transformBuildContext)
+        {
+            if ((transformBuildContext.Route.Metadata is not null) && transformBuildContext.Route.Metadata.ContainsKey(SSI_METADATA_FLAG) && (transformBuildContext.Route.Metadata[SSI_METADATA_FLAG] == SSI_METADATA_FLAG_ON))
+            {
+                transformBuildContext.AddResponseTransform(async responseContext =>
+                {
+                    if (responseContext.ProxyResponse is not null)
+                    {
+                        Stream proxyResponseContentStream = await responseContext.ProxyResponse.Content.ReadAsStreamAsync();
+
+                        using StreamReader proxyResponseContentReader = new StreamReader(proxyResponseContentStream);
+
+                        string proxyResponseContent = await proxyResponseContentReader.ReadToEndAsync();
+
+                        if (!String.IsNullOrEmpty(proxyResponseContent))
+                        {
+                            responseContext.SuppressResponseBody = true;
+
+                            byte[] proxyResponseContentBytes = Encoding.UTF8.GetBytes(proxyResponseContent);
+                            responseContext.HttpContext.Response.ContentLength = proxyResponseContentBytes.Length;
+                            await responseContext.HttpContext.Response.Body.WriteAsync(proxyResponseContentBytes);
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
