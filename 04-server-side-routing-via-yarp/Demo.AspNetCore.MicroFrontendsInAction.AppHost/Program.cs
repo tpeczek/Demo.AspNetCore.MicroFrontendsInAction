85590afcec561981306c5b77746e06a806fb923f
Tomasz Pęczek <tpeczek@gmail.com>
2024-08-01 20:39:14 +0200

    Aspireify (04-server-side-routing-via-yarp)

--- 04-server-side-routing-via-yarp/Demo.AspNetCore.MicroFrontendsInAction.AppHost/Program.cs
+++ 04-server-side-routing-via-yarp/Demo.AspNetCore.MicroFrontendsInAction.AppHost/Program.cs
@@ -3,7 +3,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var decideApplication = builder.AddProject<Projects.Demo_AspNetCore_MicroFrontendsInAction_Decide>("ca-app-decide");
 var inspireApplication = builder.AddProject<Projects.Demo_AspNetCore_MicroFrontendsInAction_Inspire>("ca-app-inspire");
 
-decideApplication.WithEnvironment("INSPIRE_SERVICE_URL", inspireApplication.GetEndpoint("https"));
-inspireApplication.WithEnvironment("DECIDE_SERVICE_URL", decideApplication.GetEndpoint("https"));
+builder.AddProject<Projects.Demo_AspNetCore_MicroFrontendsInAction_Proxy>("ca-app-proxy")
+    .WithEnvironment("DECIDE_SERVICE_URL", decideApplication.GetEndpoint("http"))
+    .WithEnvironment("INSPIRE_SERVICE_URL", inspireApplication.GetEndpoint("http"));
 
 builder.Build().Run();
