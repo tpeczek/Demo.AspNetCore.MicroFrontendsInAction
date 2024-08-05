159d6995af507da7a6341e2ee35648f5e75036d0
Tomasz Pęczek <tpeczek@gmail.com>
2024-08-05 22:53:48 +0200

    Upgrade to .NET 8 (08-composition-via-blazor-webassembly-based-web-components)

---
+++ 08-composition-via-blazor-webassembly-based-web-components/Demo.AspNetCore.MicroFrontendsInAction.Inspire/Models/RecommendationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.AspNetCore.MicroFrontendsInAction.Inspire.Models
+{
+    public class RecommendationViewModel
+    {
+        public string[] ProductsIds { get; }
+
+        public RecommendationViewModel(params string[] productsIds)
+        {
+            ProductsIds = productsIds;
+        }
+    }
+}
