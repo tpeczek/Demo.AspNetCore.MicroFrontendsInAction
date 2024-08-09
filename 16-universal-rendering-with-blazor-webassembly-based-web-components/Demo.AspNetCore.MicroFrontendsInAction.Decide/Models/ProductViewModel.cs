94d5db227690d8bd7b3a1c7cb30ef58894bc636b
Tomasz Pęczek <tpeczek@gmail.com>
2024-08-09 19:36:14 +0200

    Upgrade to .NET 8 (16-universal-rendering-with-blazor-webassembly-based-web-components)

---
+++ 16-universal-rendering-with-blazor-webassembly-based-web-components/Demo.AspNetCore.MicroFrontendsInAction.Decide/Models/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo.AspNetCore.MicroFrontendsInAction.Decide.Models
+{
+    public class ProductViewModel
+    {
+        public string Id { get; }
+
+        public string Name { get; }
+
+        public ProductViewModel(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
